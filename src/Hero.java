a1db0496593ebf7541f2d777a45511aee9a3c01b
241007 <241007@std.hi-joho.ac.jp>
2025-06-13 12:05:35 +0900

    10章終わり

--- src/Hero.java
+++ src/Hero.java
@@ -1,6 +1,12 @@
 public class Hero {
     String name = "ミナト";
     int hp = 100;
+    public Hero(String name) {
+        System.out.println(name);
+    }
+    public Hero() {
+        this("Heroのコンストラクタが動作");
+    }
     // 戦う
     public void attack(Matango m) {
         System.out.println(this.name + "の攻撃！");
--- src/SuperHero.java
+++ src/SuperHero.java
@@ -1,6 +1,10 @@
 public class SuperHero extends Hero {
     boolean flying;
     //追加の処置
+    public SuperHero() {
+        super();
+        System.out.println("SuperHeroのコンストラクタが動作");
+    }
     public void fly() {
         this.flying = true;
         System.out.println("飛び上がった！");
